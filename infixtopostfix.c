387c46d75c7f9f2c0c4dd6d13a675d6284f01593
bkbilal <bk67967@gmail.com>
2017-07-21 16:13:26 +0530

    infix postfix program

---
+++ infixtopostfix.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<ctype.h>
+typedef struct conversion
+{
+char[30];
+int top;
+}stack;
+void push
+char pop
+void convert
+int priority
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(in[])
+
+
+
+
+post[j++]=opr;
+opr=pop(&s1);
+}
+push(&s1,opr);
+push(&s1,in[i]);
+}
+else
+
